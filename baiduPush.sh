0a57dc8af945c7d565db8acbe01e435ecef414eb
Singerw <zhangsingerw@gmail.com>
2021-08-22 23:26:28 +0800

    update

---
+++ baiduPush.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 百度链接推送curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://xugaoyi.com&token=T5PEAzhGaPNbjQ2X"
+
+rm -rf urls.txt # 删除文件
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+const head = require('./config/head.js');
+const plugins = require('./config/plugins.js');
+const themeConfig = require('./config/themeConfig.js');
+
+module.exports = {
+  theme: 'vdoing', // 使用npm包主题
+  // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
+
+  title: "Singerw's Repository",
+  description: 'Singerw个人全栈技术博客,Java学习与总结。',
+  // base: '/', // 格式：'/<仓库名>/'， 默认'/'
+  markdown: {
+    lineNumbers: true, // 代码行号
+  },
+
+  head,
+  plugins,
+  themeConfig,
+}
